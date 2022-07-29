35f880427bc9f9fd91692e17e409e489efb4c078
David Wagner <dwagner86@gmail.com>
2022-07-29 12:00:55 -0400

    adds fully functional search

--- static/js/search.js
+++ static/js/search.js
@@ -15,8 +15,39 @@ function search(query) {
       keys: ['title', 'description', 'tags', 'contents']
     }
     var fuse = new Fuse(index, options);
+    var results = fuse.search(query);
 
-    var result = fuse.search(query);
-    id('searchResults').innerHTML = result.length + "<br><br>" + JSON.stringify(result);
+    // clear search results; set headers
+    id('searchResultsHeader').innerHTML = `Search: ${query}`
+    id('searchResults').innerHTML = (genResultHeaderHTML(results));
+
+    // render results
+    results.forEach(function(result) {
+      id('searchResults').innerHTML += (genResultHTML(result.item));
+    });
+
+    // show the offcanvas
+    var searchOffcanvas = new bootstrap.Offcanvas(id('searchOffcanvas'));
+    searchOffcanvas.toggle();
   });
+}
+
+// generate search result header
+function genResultHeaderHTML(results) {
+  var plural = 's';
+  if (results.length == 1) {
+    plural = '';
+  }
+
+  return `<div class="list-group-item text-center">
+  <h5 class="mb1">Found ${results.length} Recipe${plural}</h5>
+</div>`;
+}
+
+// generate search result entry
+function genResultHTML(recipe) {
+  return `<a href="${recipe.permalink}" class="list-group-item list-group-item-action">
+  <h5 class="mb-1">${recipe.title}</h5>
+  <small>${recipe.description}</small>
+</a>`;
 }
